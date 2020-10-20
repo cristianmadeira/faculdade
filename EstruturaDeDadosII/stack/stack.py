2d7d9b7b3d38f8e2afad1be687858033fb7f09bc
Cristian Madeira de Souza Pereira <cristianmadeira1106@gmail.com>
2020-10-20 09:15:55 -0300

    Remove init method of stack class

--- EstruturaDeDadosII/stack/stack.py
+++ EstruturaDeDadosII/stack/stack.py
@@ -1,8 +1,6 @@
 from baselist.baselist import BaseList
 class Stack(BaseList):
-    def __init__(self):
-        BaseList.__init__(self)
-
+    
     def insert(self,element):
         aux = [element]
         self.items = aux + self.items
